0c7e5a316c231786424ee30ff07e7d9082c78b70
Adi Narayan <adinarayanrs777@gmail.com>
2025-07-13 21:06:48 +0400

    Backend almmost done

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -14,7 +14,7 @@ const Contact = () => {
         <img className='w-full md:max-w-[480px]' src={assets.contact_img} alt="" />
         <div className='flex flex-col justify-center items-start gap-6'>
           <p className='font-semibold text-xl text-gray-600'> Our Store</p>
-          <p>Address goes here</p>
+          <p className='text-gray-700'>Starlight colive PG <br />5th main, Near Hkbk, <br/> Nagawara, Bengaluru,<br /> 560045</p>
           <p className='text-gray-700'>Phn: +91 6304656252<br />Email: hashiraenterprise@gmail.com</p>
           <p></p>
         </div>
