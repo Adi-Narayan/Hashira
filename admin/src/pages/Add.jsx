0309575db736cc69e617112c554322839be9eb2e
Adi Narayan <adinarayanrs777@gmail.com>
2025-07-25 00:12:21 +0530

    Admin Panel

--- admin/src/pages/Add.jsx
+++ admin/src/pages/Add.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react'
 import { assets } from '../assets/assets'
+import axios from 'axios'
+import { backendUrl } from '../App'
+import { toast } from 'react-toastify'
 
-const Add = () => {
+const Add = ({token}) => {
 
   const [image1,setImage1] = useState(false)
   const [image2,setImage2] = useState(false)
@@ -19,7 +22,7 @@ const Add = () => {
   const onSubmitHandler = async (e) => {
     e.preventDefault();
     try {
-      const formData = new formData()
+      const formData = new FormData()
 
       formData.append("name",name)
       formData.append("description",description)
@@ -30,15 +33,30 @@ const Add = () => {
       formData.append("sizes",JSON.stringify(sizes))
 
       image1 && formData.append("image1",image1)
-      image2 && formData.append("image2",image1)
-      image3 && formData.append("image3",image1)
-      image4 && formData.append("image4",image1)
+      image2 && formData.append("image2",image2)
+      image3 && formData.append("image3",image3)
+      image4 && formData.append("image4",image4)
 
+      const response = await axios.post(backendUrl + "/api/product/add", formData, {headers:{token}})
+
+        if(response.data.success) {
+          toast.success(response.data.message)
+          setName('')
+          setDescription('')
+          setImage1(false)
+          setImage2(false)
+          setImage3(false)
+          setImage4(false)
+          setPrice('')
+        }
+        else{
+          toast.error.message(response.data.message)
+        }
     }
     catch (error) {
-
+      console.log(error);
+      toast.error(error.message)
     }
-
   }
 
   return (
@@ -100,7 +118,7 @@ const Add = () => {
 
         <div>
           <p className='mb-2'>Product Price</p>
-          <input nChange={(e)=>setPrice(e.target.value)} value={price} className='w-full px-3 py-2 sm:w-[120px]' type="Number" placholder='500'/>
+          <input onChange={(e)=>setPrice(e.target.value)} value={price} className='w-full px-3 py-2 sm:w-[120px]' type="Number" placeholder='500'/>
         </div>
       </div>
 
@@ -132,7 +150,7 @@ const Add = () => {
 
       <div className='flex gap-2 mt-2'>
         <input onChange={()=>setBestseller(prev => !prev)} checked={bestseller} type="checkbox" id="bestseller"/>
-        <label className='cursor-pointer' htmlFor='bestseller'></label>Add to bestseller<label/>
+        <label className='cursor-pointer' htmlFor='bestseller'>Add to bestseller</label>
       </div>
 
     <button type="submit" className='w-28 py-3 mt-4 bg-black text-white'>ADD</button>
